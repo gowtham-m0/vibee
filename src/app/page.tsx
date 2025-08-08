e356e308e84fb26e6beeb9f1fabd822016253fee
Gowtham <gowthamm2712@gmail.com>
2025-08-08 22:16:09 +0530

    02-database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
 
-const Page = () =>{
+const Page = async () =>{
+  
+  
   return (
-    <div className="font-bold text-rose-500">
-      <Button>
-        Hello world
-      </Button>
-    </div>
+    <div>hello world</div>
   );
 }
 
