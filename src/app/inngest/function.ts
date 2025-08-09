33e6ae635093817b90555b19d357a327461d501d
Gowtham <gowthamm2712@gmail.com>
2025-08-10 00:08:32 +0530

    05-Ai-Jobs

--- src/app/inngest/function.ts
+++ src/app/inngest/function.ts
@@ -1,10 +1,26 @@
+import { gemini, createAgent } from '@inngest/agent-kit'
+import 'dotenv/config';
+
 import { inngest } from "./client";
+import { create } from 'domain';
+import { success } from 'zod';
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert in next.js developer. you write readable, maintainable code. you write simple next.js and react snippets.",
+      model: gemini({
+        model: "gemini-2.5-flash",
+        apiKey: process.env.GEMINI_API_KEY,
+      }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+    return { output };
   },
 );
