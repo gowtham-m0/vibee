fac0044c4f976cbe2d606f163b942731d7d709eb
Gowtham <gowthamm2712@gmail.com>
2025-12-06 16:35:08 +0530

    deployment fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,22 @@
-import { clerkMiddleware , createRouteMatcher } from '@clerk/nextjs/server';
-
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
-  "/",
+  '/',
   '/sign-in(.*)',
-  "/sign-up(.*)",
-  "/api/(.*)",
-  "/pricing(.*)"
-])
+  '/sign-up(.*)',
+  '/api/trpc(.*)',     
+  '/pricing(.*)',
+]);
 
-export default clerkMiddleware(async (auth,req)=>{
-  if(!isPublicRoute(req)){
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
     await auth.protect();
   }
 });
 
 export const config = {
   matcher: [
-    '/((?!_next|.*\\..*).*)', // everything except static
-    '/api/(?!trpc).*',        // run for API but NOT trpc
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/api/(?!trpc).*',    
   ],
 };
